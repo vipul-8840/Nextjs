9cda36a471eaf44175c804024c5c6d6be413c33e
vipul-8840 <vipulkumarlen@gmail.com>
2025-02-17 01:19:30 +0530

    learn to api handles in nextjs

--- project/app/users/page.tsx
+++ project/app/users/page.tsx
@@ -1,8 +1,29 @@
-export default function Home() {
+
+ import axios from "axios";
+ interface todo{
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+ }
+export default  async function User() {
+  
+ 
+   const response  = await fetch("https://jsonplaceholder.typicode.com/todos/");
+   const data :todo[] = await  response.json() ;
+   
+ 
     return (
-           <div>
-             User page
-           </div>
+            <div>
+               <h2>User Page</h2>
+                {data.map((todo) => (
+                  <div key={todo.id}>
+                    <p>User ID: {todo.userId}</p>
+                    <p>Title: {todo.title}</p>
+                    <hr />
+                  </div>
+                ))}
+            </div>
   
           
     );
